d7737d4fdb3f9b7797551d3f8ed784e0886aa653
Reza <srh.umir@gmail.com>
2016-03-14 18:21:23 +0100

    Report till injuries

--- Exploration.R
+++ Exploration.R
@@ -11,6 +11,7 @@ convert <- data.frame(exp = c(as.character(levels(storms$PROPDMGEXP)),"k"),
                      value = c(0,0,0,10,10,10,10,10,10,10,10,10,10,1000000000,100,100,1000,1000000,100000, 1000))
 propexp <- relable(storms$PROPDMGEXP, convert)
 cropexp <- relable(storms$CROPDMGEXP, convert)
+storms$EVTYPE <- as.character(storms$EVTYPE)
 library(plyr)
 library(dplyr)
 library(data.table)
@@ -18,12 +19,13 @@ storms <- as.data.table(storms)
 storms[, PropDamage := propexp * PROPDMG]
 storms[, CropDamage := cropexp * CROPDMG]
 storms[, TotalDamage := PropDamage + CropDamage]
-storms$EVTYPE <- as.character(storms$EVTYPE)
 
-stormsDamage <- storms[TotalDamage > 0 | FATALITIES > 0 | INJURIES,]
+harmfullStorms <- storms[TotalDamage > 0 | FATALITIES > 0 | INJURIES > 0,]
 
-DamageType <- ddply(stormsDamage, .(EVTYPE), summarise, 
-                    TotalDamage = sum(TotalDamage), 
+DamageType <- ddply(harmfullStorms, .(EVTYPE), summarise, 
+                    TotalDamage = sum(TotalDamage),
+                    PropDamage = sum(PropDamage),
+                    CropDamage = sum(CropDamage),
                     TotalFatality = sum(FATALITIES),
                     TotalInjuries = sum(INJURIES))
 #DamageType$EVTYPE <- as.character(DamageType$EVTYPE)
@@ -40,16 +42,17 @@ namelist <- c("Flood", "Thunderstorm", "TSTM", "Rain", "Tornado", "Fire",
 for ( x in namelist){
        DamageType <- sumduplicate(x, expensetable = DamageType)
 }
-library(Hmisc)
-DamageType$EVTYPE <- capitalize(tolower(DamageType$EVTYPE))
+
 #Thunderstorm and TSTM are the same
 index <- which(DamageType$EVTYPE ==  "Thunderstorm" |
                DamageType$EVTYPE == "Tstm")
-sum <- apply(DamageType[index,c(names(DamageType)[2:4])],2,sum)
+sum <- apply(DamageType[index,-1],2,sum)
 new <- data.frame("Thunderstorm", t(sum))
 names(new) <- names(DamageType)
 DamageType <- rbind(DamageType[-index,], new)
 
+library(Hmisc)
+DamageType$EVTYPE <- capitalize(tolower(DamageType$EVTYPE))
 
 
 #plot damages
@@ -57,24 +60,23 @@ DamageType <- rbind(DamageType[-index,], new)
 SevereDamage <- subset(DamageType, TotalDamage >= 10000000)
 SevereDamage <- arrange(SevereDamage, desc(TotalDamage))
 
-newdamage <- SevereDamage$TotalDamage/1e7
-newdamage <- c(newdamage[4:length(newdamage)], newdamage[1:3])
-l <- length(newdamage)
+newdamage <- SevereDamage[,c("PropDamage","CropDamage")]/1e6
+newdamage <- as.matrix(rbind(newdamage[-c(1:3),], newdamage[1:3,]))
+l <- length(newdamage[,1])
 
-barplot(c(newdamage[1:(length(newdamage)-3)],0,0,0), 
+barplot(t(rbind(newdamage[1:(l-3),],matrix(0,nrow = 3,ncol = 2))), 
         names.arg = c(SevereDamage$EVTYPE[-(1:3)], SevereDamage$EVTYPE[1:3]),
-        las =2)
-barplot(tail(newdamage,3)/15, las =2, add = T, col = "red", 
-        space = c(l-3+(l-2)*.2,.2,.2))
+        las =2, col = c("blue", "gray"))
+barplot(t(tail(newdamage,3))/15, las =2, add = T, col = c("red","gray"), 
+        space = c(l-3+(l-2)*.2,.2,.2), names.arg = "")
 title(main = "Sever damages made by natural disasters", 
-      sub = "Red bars are deviated data",
-      ylab = "Total Damage (10 Million $)")
-text(16,1300,"Red bars are associated to outliers")
-for (i in 14:(length(newdamage)-3)){
-       text(i*1.2-.5, newdamage[i]+130, round(newdamage[i], digits = 0), srt = 90)
+      ylab = "Total Damage (Million $)")
+text(16,13000,"Red bars are associated to outliers")
+for (i in 14:(l-3)){
+       text(i*1.2-.5, newdamage[i]+1300, round(sum(newdamage[i,]), digits = 0), srt = 90)
 }
-for (i in (length(newdamage)-2):length(newdamage)){
-       text(i*1.2-.5, newdamage[i]/15+150, round(newdamage[i], digits = 0), srt = 90)
+for (i in (l-2):l){
+       text(i*1.2-.5, newdamage[i]/15+1000, round(sum(newdamage[i,]), digits = 0), srt = 90)
 }
 
 
@@ -82,9 +84,10 @@ for (i in (length(newdamage)-2):length(newdamage)){
 FatInj <- subset(DamageType, TotalFatality > 30 | TotalInjuries > 100 )
 FatInj <- arrange(FatInj, desc(TotalFatality), desc(TotalInjuries))
 
+par(mar = c(8.4,4,4,4) + .1)
 barplot(t(as.matrix(rbind(FatInj[-1,c("TotalFatality", "TotalInjuries")],c(0,0)))),
         horiz = F, names.arg = c(FatInj$EVTYPE[-1],FatInj$EVTYPE[1]), las = 2, 
-        col = c("blue", "gray"), beside = F,
+        col = c("blue", "gray"), beside = F, xpd = F,
         legend.text = T, args.legend = c(xjust = 1.3))
 barplot(t(as.matrix(FatInj[1,c("TotalFatality", "TotalInjuries")]))/7, add = T, 
         las =2, names.arg = "", space = dim(FatInj)[1]*1.2 -1, 
@@ -95,6 +98,7 @@ text(dim(FatInj)[1]*1.2 -.5, FatInj$TotalFatality[1]/10 + 1400,
 text(dim(FatInj)[1]*1.2 -.5, FatInj$TotalInjuries[1]/10 + 1200,
      FatInj$TotalInjuries[1] + FatInj$TotalFatality[1], 
      srt = 90, col = "white")
+title(main = "Total number of fatalities and injuries cased by weather events 1950-2015")
 
 
 
--- sumduplicate.R
+++ sumduplicate.R
@@ -1,6 +1,6 @@
 sumduplicate <- function(pattern, expensetable){
        index <- grep(tolower(pattern), tolower(expensetable[,1]))
-       sum <- apply(expensetable[index,c(names(expensetable)[2:4])],2,sum)
+       sum <- apply(expensetable[index,-1],2,sum)
        new <- data.frame(pattern, t(sum))
        names(new) <- names(expensetable)
        rbind(expensetable[-index,],new)
